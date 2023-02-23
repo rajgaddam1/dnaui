57e1d2a96c4c8edd422a669996624cea82048799
rajgaddam1 <119797412+rajgaddam1@users.noreply.github.com>
2023-02-23 11:02:57 +0530

    Update app1.py

--- app1.py
+++ app1.py
@@ -1,8 +1,49 @@
-# Define a function to visualize the bounding boxes on the images
-def visualize(img, boxes):
-    img = np.array(img)
-    for box in boxes:
-        x1, y1, x2, y2 = box
-        img = cv2.rectangle(img, (int(x1), int(y1)), (int(x2), int(y2)), (0, 255, 0), 2)
-    img = Image.fromarray(img)
-    return img
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader
+from torchvision import transforms
+from yolov5.models.yolo import Model
+from yolov5.utils.datasets import LoadImagesAndLabels, collate_fn
+
+# Define the input and output directories
+img_dir = "path/to/preprocessed/images"
+label_dir = "path/to/preprocessed/annotations"
+
+# Define the batch size and number of workers for data loading
+batch_size = 8
+num_workers = 4
+
+# Define the transforms for data augmentation
+transforms = transforms.Compose([
+    transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.2),
+    transforms.RandomHorizontalFlip(p=0.5),
+    transforms.RandomVerticalFlip(p=0.5),
+    transforms.RandomRotation(degrees=10, resample=False, expand=False),
+    transforms.Resize((416, 416)),
+    transforms.ToTensor(),
+])
+
+# Load the images and annotations into a dataset
+dataset = LoadImagesAndLabels(img_dir, label_dir, transform=transforms)
+
+# Define the data loader
+dataloader = DataLoader(dataset, batch_size=batch_size, num_workers=num_workers, collate_fn=collate_fn)
+
+# Define the model
+model = Model(num_classes=1)
+
+# Define the optimizer and loss function
+optimizer = optim.Adam(model.parameters(), lr=0.001)
+loss_fn = nn.BCEWithLogitsLoss()
+
+# Train the model for 10 epochs
+num_epochs = 10
+for epoch in range(num_epochs):
+    for batch_idx, (images, targets, _) in enumerate(dataloader):
+        optimizer.zero_grad()
+        output = model(images)
+        loss = loss_fn(output, targets)
+        loss.backward()
+        optimizer.step()
+        print(f"Epoch {epoch+1}/{num_epochs}, Batch {batch_idx+1}/{len(dataloader)}, Loss: {loss.item():.4f}")
