2eb493431a7c9122f5eb622d661e28b5d64a5042
rajgaddam1 <119797412+rajgaddam1@users.noreply.github.com>
2023-07-09 20:31:24 +0530

    Update app1.py

--- app1.py
+++ app1.py
@@ -4,5 +4,17 @@ import spacy
 import warnings
 warnings.filterwarnings("ignore")
 st.header("Name Identification in text", anchor=None)
+def contains_name(text):
+    nlp = spacy.load('en_core_web_sm')
+    doc = nlp(text)
+    names = []
+    for ent in doc.ents:
+        if ent.label_ == 'PERSON':
+            names.append(ent.text)
+            return names, True
+    return False
 
 text = st.text_input('Enter Text',label_visibility="visible")
+if st.button("Sign In"):
+  a,b = contains_name(text):
+    
