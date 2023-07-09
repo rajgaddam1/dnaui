26a5875eb8e3edca3b2c480958c11611d83346e1
rajgaddam1 <119797412+rajgaddam1@users.noreply.github.com>
2023-07-09 19:57:00 +0530

    Update app1.py

--- app1.py
+++ app1.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import os
-import spacy_streamlit as spacy
+#import spacy_streamlit as spacy
 import warnings
 warnings.filterwarnings("ignore")
 st.header("Name Identification in text", anchor=None)
